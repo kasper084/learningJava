527715e643e13360521d90cf02672e7929596794
kasper084 <kasper084@gmail.com>
2019-02-15 15:21:51 +0200

    task 1

--- src/SayHi.java
+++
@@ -1,5 +0,0 @@
-public class SayHi {
-    public static void main(String[] args) {
-        //System.out.println("Hi!");
-    }
-}
---
+++ src/reflection/SomeClass.java
@@ -0,0 +1,19 @@
+package reflection;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SomeClass {
+    protected SomeClass() {
+        final double fdouble = 0.2324435344;
+        AtomicBoolean aboolean = new AtomicBoolean(true);
+        int [] arr = new int[10];
+        ArrayList<Integer> integerArr = new ArrayList<>();
+        String tryToChangeMe = "You`r never be able to";
+    }
+
+    private void addToArray() {
+
+    }
+
+}
